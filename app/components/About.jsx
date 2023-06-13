966d37c5b86b2ade56b72b60fe29e4a26e70aed6
Idan David <idand2000@gmail.com>
2023-06-13 21:13:28 +0300

    add sectionWrapper and end build about cmp

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,60 @@
+'use client'
 import React from 'react'
+import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+import SectionWrapper from '../hoc/SectionWrapper'
 
-const About = () => {
+const ServiceCard = ({idx, title, icon}) => {
   return (
-    <div>About</div>
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * idx, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+            }}
+            className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
+            flex justify-evenly items-center flex-col'
+        >
+          <img src={icon.src} alt={title} className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold text-center'>
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+        I'm a skilled Full Stack/Frontend Web Developer with experience in writing 
+        single page applications using the latest web technologies such as
+        React, Redux, Node.js, Express, MongoDB, TypeScript and JavaScript ES6.
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, idx) => (
+          <ServiceCard key={service.title} idx={idx} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
